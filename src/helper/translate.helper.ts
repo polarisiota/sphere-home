efb6d4d1109666e174d7f684951f047fde5018db
shawn <shawn.tan517@gmail.com>
2023-04-28 15:46:02 -0700

    update getReturnableInvoice

--- src/helper/translate.helper.ts
+++ src/helper/translate.helper.ts
@@ -1,6 +1,12 @@
 import { Invoice } from '@prisma/client';
 
 export const getReturnableInvoice = (invoice: Invoice) => {
-  // TODO convert
-  return {};
+  return {
+    id: invoice.id,
+    amount: Number(invoice.amount),
+    status: invoice.status,
+    payerId: invoice.payerId,
+    receiverId: invoice.receiverId,
+    createdAt: invoice.createdAt,
+  };
 };
